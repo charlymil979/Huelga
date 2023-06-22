9970ca8d45e97f62e991aad4962daf13d977bdd2
Carlos Contreras <86571277+charlymil979@users.noreply.github.com>
2023-06-21 22:03:55 -0300

    solucionado click en descripcion

--- docs/JS/manejo.js
+++ docs/JS/manejo.js
@@ -2,24 +2,28 @@ window.addEventListener("DOMContentLoaded", (e) => {
   document.addEventListener("click", (e) => {
  
     let $titulo = e.target.classList[0];
-    // console.log(e.target)
+    console.log(e.target)
     // console.log($titulo)
     let $seccion = document.querySelectorAll(".seccion1");
     const $flecha = document.querySelectorAll(".flecha");
     
-    if (e.target.classList.contains($titulo)){
-        $seccion.forEach((el,i) => {
-        if (el.classList.contains($titulo)) {
-          el.classList.toggle("activa");
-          $flecha[i].classList.toggle("open")
-        }else{
-          el.classList.remove("activa");
-          $flecha[i].classList.remove("open")
-        }
-        el.scrollTo(0, 50);
-      })
+    if (
+      e.target.matches("a") ||
+      e.target.matches(".flecha")
+    ) {
+      if (e.target.classList.contains($titulo)) {
+        $seccion.forEach((el, i) => {
+          if (el.classList.contains($titulo)) {
+            el.classList.toggle("activa");
+            $flecha[i].classList.toggle("open");
+          } else {
+            el.classList.remove("activa");
+            $flecha[i].classList.remove("open");
+          }
+          el.scrollTo(0, 50);
+        });
+      }
     }
-    ;
 //  window.scrollTo(0, visualViewport.height);
   });
 });
